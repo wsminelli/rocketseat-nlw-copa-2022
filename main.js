414fc6a96d05e28cb045797fd65397c186948062
Willian Minelli <wsminelli@gmail.com>
2022-11-04 17:46:17 -0300

    :tada: initial commit

---
+++ main.js
@@ -0,0 +1,52 @@
+function createGame(player1, hour, player2) {
+    return `
+        <li>
+            <figure>    
+                <img src="./assets/flags/icon-${player1}.svg" alt="Bandeira do ${player1}">
+                <figcaption>${player1}</figcaption>
+            </figure>
+            <strong>${hour}</strong>
+            <figure> 
+                <img src="./assets/flags/icon-${player2}.svg" alt="Bandeira da ${player2}">
+                <figcaption>${player2}</figcaption>
+            </figure>
+        </li>    
+    `
+}
+
+let delay = -0.4;
+function createCard(date, day, games) {
+    delay = delay + 0.4;
+    return `
+    <div class="card" style="animation-delay: ${delay}s">
+        <h2>${date}<span>${day}</span></h2>
+        <ul>
+            ${games}
+        </ul>
+
+    </div>
+    `
+}
+
+document.querySelector("#cards").innerHTML = 
+   
+    createCard('24/11', 'quinta',
+    createGame('switzerland', '07:00', 'cameroon') +
+    createGame('uruguay', '10:00', 'south korea') +
+    createGame('portugal', '13:00', 'ghana') +
+    createGame('brazil', '16:00', 'serbia')
+    ) +
+
+    createCard('28/11', 'segunda', 
+    createGame('cameroon', '07:00', 'serbia') +
+    createGame('south korea', '10:00', 'ghana') +
+    createGame('switzerland', '13:00', 'brazil') +
+    createGame('portugal', '16:00', 'uruguay')
+    ) +
+
+    createCard('02/12', 'sexta',
+    createGame('south korea', '07:00', 'portugal') +
+    createGame('ghana', '10:00', 'uruguay') +
+    createGame('serbia', '13:00', 'switzerland') + 
+    createGame('brazil', '16:00', 'cameroon')
+    )
