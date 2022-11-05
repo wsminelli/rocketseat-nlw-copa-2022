7e9cafa9ed2754abd6ba695c69a8f04399eb5d24
Willian Minelli <wsminelli@gmail.com>
2022-11-05 18:55:08 -0300

    ajuste no botão link

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 function createMenu(fase) {
     return `
         <li>
-            <svg class="button-left" fill="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 23 46" preserveAspectRatio="none">
-                <path d="M0 23C5.78 15.215 15.805 5.897 23 0v46C15.805 40.103 5.78 30.785 0 23Z"></path>
-            </svg>
-            <a href="#" class="btn btn-primary">${fase}</a>
-            <svg class="button-right" fill="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 23 46" preserveAspectRatio="none">
-                <path d="M23 23C16.749 30.785 7.43 40.103 0 46V0c7.43 5.897 16.749 15.215 23 23Z"></path>
-            </svg>
+            <a href="#">
+                <svg class="button-left" fill="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 23 46" preserveAspectRatio="none">
+                    <path d="M0 23C5.78 15.215 15.805 5.897 23 0v46C15.805 40.103 5.78 30.785 0 23Z"></path>
+                </svg>
+                <span class="btn btn-primary">${fase}</span>
+                <svg class="button-right" fill="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 23 46" preserveAspectRatio="none">
+                    <path d="M23 23C16.749 30.785 7.43 40.103 0 46V0c7.43 5.897 16.749 15.215 23 23Z"></path>
+                </svg>
+            </a>
         </li>
 
     `
