86e03e5c8965f2de637d8f55b6e1a13ab0c7674d
Willian Minelli <wsminelli@gmail.com>
2022-11-06 09:30:00 -0300

    adição de estádios

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-function createMenu(fase) {
+function createMenu(fase, nome) {
     return `
         <li>
-            <a href="#">
+            <a href="#" class="${fase}">
                 <svg class="button-left" fill="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 23 46" preserveAspectRatio="none">
                     <path d="M0 23C5.78 15.215 15.805 5.897 23 0v46C15.805 40.103 5.78 30.785 0 23Z"></path>
                 </svg>
-                <span class="btn btn-primary">${fase}</span>
+                <span class="btn btn-primary">${nome}</span>
                 <svg class="button-right" fill="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 23 46" preserveAspectRatio="none">
                     <path d="M23 23C16.749 30.785 7.43 40.103 0 46V0c7.43 5.897 16.749 15.215 23 23Z"></path>
                 </svg>
@@ -17,16 +17,16 @@ function createMenu(fase) {
 
 document.querySelector("#menu").innerHTML = `
     <ul>
-        ${createMenu("Fase de Grupos")}
-        ${createMenu("Oitavas de Final")}
-        ${createMenu("Quartas de Final")}
-        ${createMenu("Semifinal")}
-        ${createMenu("3º lugar")}
-        ${createMenu("Final")}
+        ${createMenu("fase-grupo", "Fase de Grupos")}
+        ${createMenu("fase-oitavas", "Oitavas de Final")}
+        ${createMenu("fase-quartas", "Quartas de Final")}
+        ${createMenu("fase-semifinal", "Semifinal")}
+        ${createMenu("fase-terceiro", "3º lugar")}
+        ${createMenu("fase-final", "Final")}
     </ul>
 `
 
-function createGame(player1, hour, player2) {
+function createGame(player1, stadium, hour, player2) {
     return `
         <li>
             <figure>    
@@ -34,7 +34,7 @@ function createGame(player1, hour, player2) {
                 <figcaption>${player1}</figcaption>
             </figure>
             <div class="info">
-                <span>Al Bayt Stadium<br></span>
+                <span>${stadium}<br></span>
                 <strong>${hour}</strong>
             </div>
             <figure> 
@@ -46,10 +46,10 @@ function createGame(player1, hour, player2) {
 }
 
 let delay = -0.4;
-function createCard(date, day, games) {
+function createCard(fase, date, day, games) {
     delay = delay + 0.4;
     return `
-    <div class="card swiper-slide" style="animation-delay: ${delay}s">
+    <div class="card swiper-slide ${fase}" style="animation-delay: ${delay}s">
         <h2>${date}<span>${day}</span></h2>
         <ul>
             ${games}
@@ -62,102 +62,158 @@ function createCard(date, day, games) {
 document.querySelector("#cards").innerHTML = `
     <div class="swiper mySwiper">
         <div class="swiper-wrapper">
-            ${createCard("20/11", "domingo", createGame("qatar", "13:00", "ecuador"))}
-            ${createCard(
+            ${createCard("grupo", "20/11", "domingo", createGame("qatar", "AL BAYT", "13:00", "ecuador"))}
+            ${createCard("grupo", 
                 "21/11",
                 "segunda",
-                createGame("england", "10:00", "iran") +
-                createGame("senegal", "13:00", "netherlands") +
-                createGame("united states", "16:00", "wales")
+                createGame("england", "INTERNACIONAL KHALIFA", "10:00", "iran") +
+                createGame("senegal", "AL THUMAMA", "13:00", "netherlands") +
+                createGame("united states", "AHMAD BIN ALI", "16:00", "wales")
             )}
-            ${createCard(
+            ${createCard("grupo", 
                 "22/11",
                 "terça",
-                createGame("argentina", "07:00", "saudi arabia") +
-                createGame("denmark", "10:00", "tunisia") +
-                createGame("mexico", "13:00", "poland") +
-                createGame("france", "16:00", "australia")
+                createGame("argentina", "LUSAIL", "07:00", "saudi arabia") +
+                createGame("denmark", "CIDADE DA EDUCAÇÃO", "10:00", "tunisia") +
+                createGame("mexico", "ESTÁDIO 974", "13:00", "poland") +
+                createGame("france", "AL JANOUB", "16:00", "australia")
             )}
-            ${createCard(
+            ${createCard("grupo", 
                 "23/11",
                 "quarta",
-                createGame("morocco", "07:00", "croatia") +
-                createGame("germany", "10:00", "japan") +
-                createGame("spain", "13:00", "costa rica") +
-                createGame("belgium", "16:00", "canada")
+                createGame("morocco", "AL BAYT", "07:00", "croatia") +
+                createGame("germany", "INTERNACIONAL KHALIFA", "10:00", "japan") +
+                createGame("spain", "AL THUMAMA", "13:00", "costa rica") +
+                createGame("belgium", "AHMAD BIN ALI","16:00", "canada")
             )}
-            ${createCard(
+            ${createCard("grupo", 
                 "24/11",
                 "quinta",
-                createGame("switzerland", "07:00", "cameroon") +
-                createGame("uruguay", "10:00", "south korea") +
-                createGame("portugal", "13:00", "ghana") +
-                createGame("brazil", "16:00", "serbia")
+                createGame("switzerland", "AL JANOUB", "07:00", "cameroon") +
+                createGame("uruguay", "CIDADE DA EDUCAÇÃO", "10:00", "south korea") +
+                createGame("portugal", "ESTÁDIO 974", "13:00", "ghana") +
+                createGame("brazil", "LUSAIL", "16:00", "serbia")
             )}
-            ${createCard(
+            ${createCard("grupo", 
                 "25/11",
                 "sexta",
-                createGame("wales", "07:00", "iran") +
-                createGame("qatar", "10:00", "senegal") +
-                createGame("netherlands", "13:00", "ecuador") +
-                createGame("england", "16:00", "united states")
+                createGame("wales", "AHMAD BIN ALI", "07:00", "iran") +
+                createGame("qatar", "AL THUMAMA", "10:00", "senegal") +
+                createGame("netherlands", " INTERNACIONAL KHALIFA ", "13:00", "ecuador") +
+                createGame("england", "AL BAYT", "16:00", "united states")
             )}
-            ${createCard(
+            ${createCard("grupo", 
                 "26/11",
                 "sábado",
-                createGame("tunisia", "07:00", "australia") +
-                createGame("poland", "10:00", "saudi arabia") +
-                createGame("france", "13:00", "denmark") +
-                createGame("argentina", "16:00", "mexico")
+                createGame("tunisia", "AL JANOUB", "07:00", "australia") +
+                createGame("poland", "CIDADE DA EDUCAÇÃO", "10:00", "saudi arabia") +
+                createGame("france", "ESTÁDIO 974", "13:00", "denmark") +
+                createGame("argentina", "LUSAIL", "16:00", "mexico")
             )}
-            ${createCard(
+            ${createCard("grupo", 
                 "27/11",
                 "domingo",
-                createGame("japan", "07:00", "costa rica") +
-                createGame("belgium", "10:00", "morocco") +
-                createGame("croatia", "13:00", "canada") +
-                createGame("spain", "16:00", "germany")
+                createGame("japan", "AHMAD BIN ALI", "07:00", "costa rica") +
+                createGame("belgium", "AL THUMAMA", "10:00", "morocco") +
+                createGame("croatia", "INTERNACIONAL KHALIFA", "13:00", "canada") +
+                createGame("spain", "AL BAYT", "16:00", "germany")
             )}
-            ${createCard(
+            ${createCard("grupo", 
                 "28/11",
                 "segunda",
-                createGame("cameroon", "07:00", "serbia") +
-                createGame("south korea", "10:00", "ghana") +
-                createGame("brazil", "13:00", "switzerland") +
-                createGame("portugal", "16:00", "uruguay")
+                createGame("cameroon", "AL JANOUB", "07:00", "serbia") +
+                createGame("south korea", "CIDADE DA EDUCAÇÃO", "10:00", "ghana") +
+                createGame("brazil", "ESTÁDIO 974", "13:00", "switzerland") +
+                createGame("portugal", "LUSAIL", "16:00", "uruguay")
             )}
-            ${createCard(
+            ${createCard("grupo", 
                 "29/11",
                 "terça",
-                createGame("ecuador", "12:00", "senegal") +
-                createGame("netherlands", "12:00", "qatar") +
-                createGame("iran", "16:00", "united states") +
-                createGame("wales", "16:00", "england")
+                createGame("ecuador", "INTERNACIONAL KHALIFA", "12:00", "senegal") +
+                createGame("netherlands", "AL BAYT", "12:00", "qatar") +
+                createGame("iran", "AL THUMAMA", "16:00", "united states") +
+                createGame("wales", "AHMAD BIN ALI", "16:00", "england")
             )}
-            ${createCard(
+            ${createCard("grupo", 
                 "30/11",
                 "quarta",
-                createGame("tunisia", "12:00", "france") +
-                createGame("australia", "12:00", "denmark") +
-                createGame("poland", "16:00", "argentina") +
-                createGame("saudi arabia", "16:00", "mexico")
+                createGame("tunisia", "CIDADE DA EDUCAÇÃO", "12:00", "france") +
+                createGame("australia", "AL JANOUB", "12:00", "denmark") +
+                createGame("poland", "STÁDIO 974 ", "16:00", "argentina") +
+                createGame("saudi arabia", "LUSAIL", "16:00", "mexico")
             )}
-            ${createCard(
+            ${createCard("grupo", 
                 "01/12",
                 "quinta",
-                createGame("croatia", "12:00", "belgium") +
-                createGame("canada", "12:00", "morocco") +
-                createGame("japan", "16:00", "spain") +
-                createGame("costa rica", "16:00", "germany")
+                createGame("croatia", "AHMAD BIN ALI", "12:00", "belgium") +
+                createGame("canada", "AL THUMAMA", "12:00", "morocco") +
+                createGame("japan", "INTERNACIONAL KHALIFA", "16:00", "spain") +
+                createGame("costa rica", "AL BAYT ", "16:00", "germany")
             )}
-            ${createCard(
+            ${createCard("grupo", 
                 "02/12",
                 "sexta",
-                createGame("south korea", "12:00", "portugal") +
-                createGame("ghana", "12:00", "uruguay") +
-                createGame("serbia", "16:00", "switzerland") +
-                createGame("cameroon", "16:00", "brazil")
-            )}    
+                createGame("south korea", "AL JANOUB", "12:00", "portugal") +
+                createGame("ghana", "CIDADE DA EDUCAÇÃO", "12:00", "uruguay") +
+                createGame("serbia", "ESTÁDIO 974", "16:00", "switzerland") +
+                createGame("cameroon", "LUSAIL", "16:00", "brazil")
+            )}
+            ${createCard("oitavas", 
+                "03/12",
+                "sábado",
+                createGame("unknown", "INTERNACIONAL KHALIFA", "12:00", "unknown") +
+                createGame("unknown", "AHMAD BIN ALI", "16:00", "unknown")
+            )}
+            ${createCard("oitavas", 
+                "04/12",
+                "domingo",
+                createGame("unknown", "AL THUMAMA", "12:00", "unknown") +
+                createGame("unknown", "AL BAYT", "16:00", "unknown")
+            )}
+            ${createCard("oitavas", 
+                "05/12",
+                "segunda",
+                createGame("unknown", "AL JANOUB", "12:00", "unknown") +
+                createGame("unknown", "ESTÁDIO 974", "16:00", "unknown")
+            )}
+            ${createCard("oitavas", 
+                "06/12",
+                "terça",
+                createGame("unknown", "CIDADE DA EDUCAÇÃO", "12:00", "unknown") +
+                createGame("unknown", "LUSAIL", "16:00", "unknown")
+            )}
+            ${createCard("quartas", 
+                "09/12",
+                "sexta",
+                createGame("unknown", "CIDADE DA EDUCAÇÃO", "12:00", "unknown") +
+                createGame("unknown", "LUSAIL", "16:00", "unknown")
+            )}
+            ${createCard("quartas", 
+                "10/12",
+                "sábado",
+                createGame("unknown", "AL THUMAMA", "12:00", "unknown") +
+                createGame("unknown", "AL BAYT", "16:00", "unknown")
+            )} 
+            ${createCard("semifinal", 
+                "13/12",
+                "terça",
+                createGame("unknown", "LUSAIL", "16:00", "unknown")
+            )} 
+            ${createCard("semifinal", 
+                "14/12",
+                "quarta",
+                createGame("unknown", "AL BAYT", "16:00", "unknown")
+            )}
+            ${createCard("terceiro", 
+                "17/12",
+                "sábado",
+                createGame("unknown", "INTERNACIONAL KHALIFA", "12:00", "unknown")
+            )}
+            ${createCard("final", 
+                "18/12",
+                "domingo",
+                createGame("unknown", "LUSAIL", "12:00", "unknown")
+            )}          
         </div>
         <div class="swiper-button-next">
             <img src="./assets/arrowRight.svg">
@@ -167,3 +223,4 @@ document.querySelector("#cards").innerHTML = `
         </div>
     </div>
 `
+
