4826fe28cfa8fd9c395e8190309b663a7e05523d
Willian Minelli <wsminelli@gmail.com>
2022-11-06 22:26:40 -0300

    melhorias

---
+++ final.js
@@ -0,0 +1,70 @@
+function createMenu(link, fase, nome) {
+    return `
+        <li>
+            <a href="./${link}.html" class="${fase}">
+                <svg class="button-left" fill="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 23 46" preserveAspectRatio="none">
+                    <path d="M0 23C5.78 15.215 15.805 5.897 23 0v46C15.805 40.103 5.78 30.785 0 23Z"></path>
+                </svg>
+                <span class="btn btn-primary">${nome}</span>
+                <svg class="button-right" fill="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 23 46" preserveAspectRatio="none">
+                    <path d="M23 23C16.749 30.785 7.43 40.103 0 46V0c7.43 5.897 16.749 15.215 23 23Z"></path>
+                </svg>
+            </a>
+        </li>
+
+    `
+}
+
+document.querySelector("#menu").innerHTML = `
+    <ul>
+        ${createMenu("index", "fase-grupo", "Fase de Grupos")}
+        ${createMenu("oitavas", "fase-oitavas", "Oitavas de Final")}
+        ${createMenu("quartas", "fase-quartas", "Quartas de Final")}
+        ${createMenu("semifinal", "fase-semifinal", "Semifinal")}
+        ${createMenu("terceiro-lugar", "fase-terceiro", "3º lugar")}
+        ${createMenu("final", "fase-final", "Final")}
+    </ul>
+`
+
+function createGame(player1, stadium, hour, player2) {
+    return `
+        <li>
+            <figure>    
+                <img src="./assets/flags/icon-${player1}.svg" alt="Bandeira do ${player1}">
+                <figcaption>${player1}</figcaption>
+            </figure>
+            <div class="info">
+                <span>${stadium}<br></span>
+                <strong>${hour}</strong>
+            </div>
+            <figure> 
+                <img src="./assets/flags/icon-${player2}.svg" alt="Bandeira da ${player2}">
+                <figcaption>${player2}</figcaption>
+            </figure>
+        </li>    
+    `
+}
+
+let delay = -0.4;
+function createCard(fase, date, day, games) {
+    delay = delay + 0.4;
+    return `
+    <div class="card ${fase}" style="animation-delay: ${delay}s">
+        <h2>${date}<span>${day}</span></h2>
+        <ul>
+            ${games}
+        </ul>
+
+    </div>
+    `
+}
+
+document.querySelector("#cards").innerHTML = `
+    <div class="games">
+        ${createCard("final", 
+        "18/12",
+        "domingo",
+        createGame("unknown", "LUSAIL", "12:00", "unknown")
+        )} 
+    </div>
+`
--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-function createMenu(fase, nome) {
+function createMenu(link, fase, nome) {
     return `
         <li>
-            <a href="#" class="${fase}">
+            <a href="./${link}.html" class="${fase}">
                 <svg class="button-left" fill="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 23 46" preserveAspectRatio="none">
                     <path d="M0 23C5.78 15.215 15.805 5.897 23 0v46C15.805 40.103 5.78 30.785 0 23Z"></path>
                 </svg>
@@ -16,13 +16,13 @@ function createMenu(fase, nome) {
 }
 
 document.querySelector("#menu").innerHTML = `
-    <ul>
-        ${createMenu("fase-grupo", "Fase de Grupos")}
-        ${createMenu("fase-oitavas", "Oitavas de Final")}
-        ${createMenu("fase-quartas", "Quartas de Final")}
-        ${createMenu("fase-semifinal", "Semifinal")}
-        ${createMenu("fase-terceiro", "3º lugar")}
-        ${createMenu("fase-final", "Final")}
+    <ul class="nav-menu">
+        ${createMenu("index", "fase-grupo", "Fase de Grupos")}
+        ${createMenu("oitavas", "fase-oitavas", "Oitavas de Final")}
+        ${createMenu("quartas", "fase-quartas", "Quartas de Final")}
+        ${createMenu("semifinal", "fase-semifinal", "Semifinal")}
+        ${createMenu("terceiro-lugar", "fase-terceiro", "3º lugar")}
+        ${createMenu("final", "fase-final", "Final")}
     </ul>
 `
 
@@ -157,63 +157,7 @@ document.querySelector("#cards").innerHTML = `
                 createGame("ghana", "CIDADE DA EDUCAÇÃO", "12:00", "uruguay") +
                 createGame("serbia", "ESTÁDIO 974", "16:00", "switzerland") +
                 createGame("cameroon", "LUSAIL", "16:00", "brazil")
-            )}
-            ${createCard("oitavas", 
-                "03/12",
-                "sábado",
-                createGame("unknown", "INTERNACIONAL KHALIFA", "12:00", "unknown") +
-                createGame("unknown", "AHMAD BIN ALI", "16:00", "unknown")
-            )}
-            ${createCard("oitavas", 
-                "04/12",
-                "domingo",
-                createGame("unknown", "AL THUMAMA", "12:00", "unknown") +
-                createGame("unknown", "AL BAYT", "16:00", "unknown")
-            )}
-            ${createCard("oitavas", 
-                "05/12",
-                "segunda",
-                createGame("unknown", "AL JANOUB", "12:00", "unknown") +
-                createGame("unknown", "ESTÁDIO 974", "16:00", "unknown")
-            )}
-            ${createCard("oitavas", 
-                "06/12",
-                "terça",
-                createGame("unknown", "CIDADE DA EDUCAÇÃO", "12:00", "unknown") +
-                createGame("unknown", "LUSAIL", "16:00", "unknown")
-            )}
-            ${createCard("quartas", 
-                "09/12",
-                "sexta",
-                createGame("unknown", "CIDADE DA EDUCAÇÃO", "12:00", "unknown") +
-                createGame("unknown", "LUSAIL", "16:00", "unknown")
-            )}
-            ${createCard("quartas", 
-                "10/12",
-                "sábado",
-                createGame("unknown", "AL THUMAMA", "12:00", "unknown") +
-                createGame("unknown", "AL BAYT", "16:00", "unknown")
-            )} 
-            ${createCard("semifinal", 
-                "13/12",
-                "terça",
-                createGame("unknown", "LUSAIL", "16:00", "unknown")
-            )} 
-            ${createCard("semifinal", 
-                "14/12",
-                "quarta",
-                createGame("unknown", "AL BAYT", "16:00", "unknown")
-            )}
-            ${createCard("terceiro", 
-                "17/12",
-                "sábado",
-                createGame("unknown", "INTERNACIONAL KHALIFA", "12:00", "unknown")
-            )}
-            ${createCard("final", 
-                "18/12",
-                "domingo",
-                createGame("unknown", "LUSAIL", "12:00", "unknown")
-            )}          
+            )}         
         </div>
         <div class="swiper-button-next">
             <img src="./assets/arrowRight.svg">
@@ -224,3 +168,12 @@ document.querySelector("#cards").innerHTML = `
     </div>
 `
 
+let nav = document.querySelector('#menu');
+let menu = document.querySelector('.menu-mobile')
+
+function abrirMenu() {
+  nav.classList.toggle('abrirMenu');
+  menu.classList.toggle('abrirMenu');
+}
+
+menu.onclick = abrirMenu;
